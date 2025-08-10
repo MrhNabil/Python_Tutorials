2dd9d2d074dc81d06c67e8884800783f0a1626fe
Mrh Nabil <rakib.nabil@northsouth.edu>
2025-08-10 14:20:56 +0600

    commit

---
+++ if_statement_1.py
@@ -0,0 +1,12 @@
+response = input("Are you angry (yes/no/maybe): ")
+
+if response == "yes":
+    print("You need to stay out from a gun or you might use it!")
+elif response == "no":
+    print("Good! then call your ex. Your mental peace is not necessary.") 
+elif response == "maybe":
+    print("WOW!! you don't even know how are you feeling. You donkey!")
+elif response == "":
+    print("What? You can not write!!? Get some education first.")
+      
+
