3b99c4101b6da90093492eff66173b3d506ce95f
Mrh Nabil <rakib.nabil@northsouth.edu>
2025-08-12 17:11:29 +0600

    commit

---
+++ logical_operator.py
@@ -0,0 +1,32 @@
+# logical operator help us to evaluate multiple conditions (or, and, not)
+#       or  = at least one condition must be true
+#       and = both conditions must be true
+#       not = inverts the condition
+'''
+temp = 25
+is_raining = False
+
+if temp == 25 and is_raining:
+    print("Program is canceled!")
+else:
+    print("Let's go!!")
+'''
+
+
+temp = float(input("Enter the temperature: "))
+is_raining = False
+
+
+if temp > 35 or temp < 0 or is_raining:
+    print("Program is canceled!")
+else:
+    print("Have to go!")
+
+
+
+
+
+
+
+
+
