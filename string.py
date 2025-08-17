2a48c4035eaa1ad0d935682dbffa3b5c51ac2253
Mrh Nabil <rakib.nabil@northsouth.edu>
2025-08-17 13:27:32 +0600

    commit

---
+++ string.py
@@ -0,0 +1,20 @@
+
+#name = input("Enter your Full name: ")
+#phone_number = input("Enter you phone #: ")
+
+#result = len(name)
+#result = name.find("l")    #  the counting index will start from zero and .find("") will find when a character 1st occured
+#result = name.rfind("g")   # rfind is reverse find and will search occurance of a character
+
+#name = name.capitalize()   # It will make uppercase the 1st latter, if there is multiple words that doesn't matter. It will only capitalize 1st one
+#name = name.upper()        # it will make all letters upper case
+#name = name.lower()        # it will make all letters lower case
+#result = name.isdigit()    # return a "True" function if my input is all in digit else "False"
+#result = name.isalpha()     # return "True" if my name is all alphabet
+#phone_number = phone_number.count("-")
+#phone_number = phone_number.replace("-", " ")
+
+
+#print(phone_number)
+
+#print(help(str))  #it will show all the comprehensive string method available
